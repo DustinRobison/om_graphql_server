7c63cc32bc287c4df5376636a002f5cd88b50aad
Dustin Robison <dustin.robison@viasat.com>
2020-08-12 09:45:44 -0700

    chore: prettier orders

--- src/providers/orders.ts
+++ src/providers/orders.ts
@@ -3,31 +3,31 @@ import { Order, QueryOrderArgs } from '../generated/graphql';
 
 const orders: Order[] = [
   {
-    id: "123",
+    id: '123',
     orderLines: [],
-    customerRelationshipId: "abc",
-    executionDate: "2020-01-01T00:00:00.000Z",
-    state: "new"
+    customerRelationshipId: 'abc',
+    executionDate: '2020-01-01T00:00:00.000Z',
+    state: 'new'
   },
   {
-    id: "234",
+    id: '234',
     orderLines: [],
-    customerRelationshipId: "abc",
-    executionDate: "2020-01-01T00:00:00.000Z",
-    state: "new"
+    customerRelationshipId: 'abc',
+    executionDate: '2020-01-01T00:00:00.000Z',
+    state: 'new'
   },
   {
-    id: "34",
+    id: '34',
     orderLines: [],
-    customerRelationshipId: "abc",
-    executionDate: "2020-01-01T00:00:00.000Z",
-    state: "new"
-  },
-  ];
+    customerRelationshipId: 'abc',
+    executionDate: '2020-01-01T00:00:00.000Z',
+    state: 'new'
+  }
+];
 
 export class OrdersProvider extends DataSource {
   public async getOrder(args: QueryOrderArgs) {
-    return orders.find(order => order.id === args.id);
+    return orders.find((order) => order.id === args.id);
   }
 
   public async getOrders() {
